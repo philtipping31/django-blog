37a601fc6e6c17760de5bb3556c4c50a86b6fc58
Phil Tipping <philtipping3110@gmail.com>
2024-05-22 09:18:37 +0000

    Add a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-philtipping3-djangoblog-2libv0akzh6.ws-eu111.gitpod.io',
+ALLOWED_HOSTS = ['8000-philtipping3-djangoblog-2libv0akzh6.ws-eu114.gitpod.io',
                 '.herokuapp.com']
 
 
@@ -90,6 +90,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
