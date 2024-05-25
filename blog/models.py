fab312c77e6c3a8f9b73e5eb287bf0043b2fbf8c
Phil Tipping <philtipping3110@gmail.com>
2024-05-25 13:32:09 +0000

    Push code for tutor support

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     update_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | Written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -23,4 +29,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now=True)
-    
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"    
