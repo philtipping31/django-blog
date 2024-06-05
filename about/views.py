c49c7436ffda13569b0d1daf33a1284965ae495a
Phil Tipping <philtipping3110@gmail.com>
2024-06-05 17:10:34 +0000

    ready for production

--- about/views.py
+++ about/views.py
@@ -6,13 +6,23 @@ from .forms import CollaborateForm
 # Create your views here.
 def about_me(request):
     """
-    Renders the about page
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+    Displays an individual instance of :model:`about.About`.
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`.
+    
+    **Template**
+    :template:`about/about.html`
     """
 
     if request.method == "POST":
-        collaborate_from = CollaborateForm(data=request.POST)
-        if collaborate_from.is_valid():
-            collaborate_from.save()
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
             messages.add_message(request, messages.SUCCESS,
             "Collaboration request received! I endeavor to respond in 2 working days.")
 
